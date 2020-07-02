c3ea23ab21bc1a884468b2bf01b2c7fe8e210ec2
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 14:25:42 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,26 @@
 class Doctor
-  attr_accessor
+  attr_accessor :name, :patient
   
+  @@all  []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, patient)
+    
+  end
+  
+  def appointments
+    Appointment.all.select {|a| a.doctor == self}
+  end
+  
+  def patients
+    self.appointments.all.collect {|a| a.patient}
+  end
+  
+  def self.all
+    @@all
+  end
 end
